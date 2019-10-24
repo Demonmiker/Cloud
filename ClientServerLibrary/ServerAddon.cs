241a5ec88326c838ec798069241cb5ae8f0ce452
veldm <Dmavel8@yandex.ru>
2019-10-24 22:36:22 +0300

    Пофиксил хэндлеры в файле ServerAddon.cs

--- ClientServerLibrary/ServerAddon.cs
+++ ClientServerLibrary/ServerAddon.cs
@@ -10,43 +10,70 @@ namespace ClientServerLibrary
     {
         void HandleDelete()
         {
+            FileInfo FI = null;
+            DirectoryInfo DI = null;
             string path = br.ReadString();
             path = "ServerData/" + path;
-            FileInfo fi = new FileInfo(path);
-            try
-            {
-                fi.Delete();
-                bw.Write("Файл успешно удалён");
-            }
-            catch (Exception E) { bw.Write($"Не удалось удалить файл - {E.Message}"); }
+            if (path.Contains('.')) FI = new FileInfo(path);
+            else DI = new DirectoryInfo(path);
+            if (FI != null) try
+                {
+                    FI.Delete();
+                    bw.Write("Файл успешно удалён");
+                }
+                catch (Exception E) { bw.Write($"Не удалось переместить файл - {E.Message}"); }
+            else try
+                {
+                    DI.Delete();
+                    bw.Write("Директория успешно удалена");
+                }
+                catch (Exception E) { bw.Write($"Не удалось переместить директорию - {E.Message}"); }
             cs.Send(ms_buf);
         }
 
         void HandleMove()
         {
+            FileInfo FI = null;
+            DirectoryInfo DI = null;
             string path = br.ReadString();
             path = "ServerData/" + path;
-            FileInfo fi = new FileInfo(path);
-            try
-            {
-                fi.MoveTo(br.ReadString());
-                bw.Write("Файл успешно перемещён");
-            }
-            catch (Exception E) { bw.Write($"Не удалось переместить файл - {E.Message}"); }
+            if (path.Contains('.')) FI = new FileInfo(path);
+            else DI = new DirectoryInfo(path);
+            if (FI != null) try
+                {
+                    FI.MoveTo($"{br.ReadString()}/{FI.Name}");
+                    bw.Write("Файл успешно перемещён");
+                }
+                catch (Exception E) { bw.Write($"Не удалось переместить файл - {E.Message}"); }
+            else try
+                {
+                    DI.MoveTo($"{br.ReadString()}/{DI.Name}");
+                    bw.Write("Директория успешно перемещена");
+                }
+                catch (Exception E) { bw.Write($"Не удалось переместить директорию - {E.Message}"); }
             cs.Send(ms_buf);
         }
 
         void HandleRename()
         {
+            FileInfo FI = null;
+            DirectoryInfo DI = null;
             string path = br.ReadString();
             path = "ServerData/" + path;
-            FileInfo fi = new FileInfo(path);
-            try
-            {
-                fi.MoveTo($"{(fi.DirectoryName == "ServerData" ? "" : fi.DirectoryName)}/{br.ReadString()}");
-                bw.Write("Файл успешно переименован");
-            }
-            catch (Exception E) { bw.Write($"Не удалось переименовать файл - {E.Message}"); }
+            if (path.Contains('.')) FI = new FileInfo(path);
+            else DI = new DirectoryInfo(path);
+            if (FI != null) try
+                {
+                    FI.MoveTo($"{(FI.DirectoryName == "ServerData" ? "" : FI.DirectoryName)}/{br.ReadString()}");
+                    bw.Write("Файл успешно переименован");
+                }
+                catch (Exception E) { bw.Write($"Не удалось переименовать файл - {E.Message}"); }
+            else try
+                {
+                    DI.MoveTo($"{DI.Parent.Name}/{br.ReadString()}");
+                    bw.Write("Директория успешно переименована");
+                }
+                catch (Exception E) { bw.Write($"Не удалось переименовать директорию - {E.Message}"); }
             cs.Send(ms_buf);
         }
 
