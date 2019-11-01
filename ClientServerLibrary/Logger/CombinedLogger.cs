d23bf7b70478dc8a54b83b7486280809ca8f9e7e
veldm <40839823+veldm@users.noreply.github.com>
2019-11-01 20:26:45 +0300

    Добавил запись логов в файл и комбинированный логгер

---
+++ ClientServerLibrary/Logger/CombinedLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientServerLibrary.Logger
+{
+    class CombinedLogger : ILogger
+    {
+        ILogger[] Loggers;
+
+        public CombinedLogger(ILogger[] Loggers) { }
+
+        public void Write(String S)
+        {
+            foreach (ILogger logger in Loggers)
+                logger.Write(S);
+        }
+
+        public void WriteLine(String S)
+        {
+            foreach (ILogger logger in Loggers)
+                logger.WriteLine(S);
+        }
+    }
+}
---
+++ ClientServerLibrary/Logger/ToFileLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ClientServerLibrary.Logger
+{
+    class ToFileLogger : ILogger
+    {
+        StreamWriter SW;
+
+        public ToFileLogger()
+        {
+            FileStream FS = new FileStream("Log.log", FileMode.OpenOrCreate);
+            SW = new StreamWriter(FS);
+        }
+
+        public void Write(String S)
+        {
+            SW.Write($"{DateTime.Now}: {S}");
+        }
+
+        public void WriteLine(String S)
+        {
+            SW.WriteLine($"{DateTime.Now}: {S}");
+        }
+    }
+}
