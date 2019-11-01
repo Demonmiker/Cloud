6972964f56e8bf610f88dde9bb7cce10ea2ae54e
veldm <40839823+veldm@users.noreply.github.com>
2019-11-01 21:33:26 +0300

    Добавил говорилку

---
+++ ClientServerLibrary/Logger/VoiceLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CognitiveServices.Speech;
+
+namespace ClientServerLibrary.Logger
+{
+    class VoiceLogger : ILogger
+    {
+        SpeechSynthesizer SS;
+
+        public VoiceLogger()
+        {
+            SS = new SpeechSynthesizer(SpeechConfig.FromSubscription
+                ("YourSubscriptionKey", "YourServiceRegion"));
+        }
+
+        public void Write(String S)
+        {
+            SS.SpeakTextAsync(S);
+        }
+
+        public void WriteLine(String S)
+        {
+            SS.SpeakTextAsync(S);
+        }
+    }
+}
